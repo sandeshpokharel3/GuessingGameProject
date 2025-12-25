8c8b23d1ccfd26ba61d5140efaf57b05399201e4
sandeshpokharel3 <sandeshpokhar9@gmail.com>
2025-12-25 12:39:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
 package com.sandesh.guessinggame;
 
-import java.util.Scanner;
-public class Main {
+import java.util.Scanner;  //scanner is used to take input from user which was import from java standard library!
 
+public class Main {
+//defines a public class main
+    
     public static void main(String[] args) {
+        // This is a entry point means java start executing code from here
         // mini project for guessing random number
+        
         Scanner sc = new Scanner(System.in);
         int myNumber = (int) (Math.random() * 100);
         int userNumber = 0;
 
-        do {
-            System.out.println("Guess my number(1-100) :");
-            userNumber = sc.nextInt();
+        do { //do-while loop runs at least once no matter what we write in code
+            
+            System.out.println("Guess my number(0-100) :");
+            userNumber = sc.nextInt(); //Reads an integer typed by user
 
             if (userNumber == myNumber) {
                 System.out.println("wow....That's correct!");
-                break;
+                break; //Exits the loop immediately, game ends!
+                
             } else if (userNumber > myNumber) {
                 System.out.println("your number is too large");
 
@@ -24,9 +30,10 @@ public class Main {
             }
 
 
-        } while (userNumber >= 0);
+        } while (userNumber >= 0); //loop continues if user enter non- negative number
 
         System.out.println("my number was : ");
         System.out.println(myNumber);
-    }
-}
+        
+    } //This closes main method
+}//This closes main class
